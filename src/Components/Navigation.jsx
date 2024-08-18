d8410182961e8558258996a6982aa35f4cf60182
zhxnlee <zhenxuan25@gmail.com>
2024-08-18 20:32:46 +0800

    changes for mobile responsive

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,9 +1,25 @@
 
-import React from "react"
+import React, {useState, useEffect} from "react"
 import styles from "./Navigation.module.css"
 import {Link} from 'react-scroll';
-
+import { TiThMenu } from "react-icons/ti";
 function Navigation(){
+
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 600);
+    const [menu, setMenu] = useState(false);
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth < 600);
+        };
+
+        console.log("Mobile");
+        window.addEventListener("resize", handleResize);
+
+        // Cleanup event listener on component unmount
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    
     const links = [
         {name: "Introduction", to: "introduction"},
         {name: "About me", to: "aboutMe"},
@@ -19,17 +35,37 @@ function Navigation(){
     ));
 
     return (
-        <div className = {styles.navigationSection}>
-            <div className = {styles.logo}>
-                <h1>ZX</h1>
-            </div>
-            <div className = {styles.navigationList}>
-                <ul>
-                    {linkItems}
-                </ul>
-            </div>
-        </div>
-    )
+        <>
+            {!isSmallScreen ? (
+                <div className={styles.navigationSection}>
+                    <div className={styles.logo}>
+                        <h1>ZX</h1>
+                    </div>
+                    <div className={styles.navigationList}>
+                        <ul>{linkItems}</ul>
+                    </div>
+                </div>
+            ) : (
+                <>
+                    <div className = {styles.navigationMobileSection}>
+                        <div className = {styles.mobileTitle}>
+                            <h1>ZX</h1>
+                        </div>
+                    {/* <button onClick={() => setMenu(!menu)} className={styles.menuButton}> */}
+                    
+                        {/* Menu
+                    </button> */}
+                    <TiThMenu  onClick={() => setMenu(!menu)} className={styles.menuButton}/>
+                    {menu && (
+                        <div className={styles.navigationMobile}>
+                            <ul>{linkItems}</ul>
+                        </div>
+                    )}
+                    </div>
+                </>
+            )}
+        </>
+    );
 }
 
 export default Navigation;
